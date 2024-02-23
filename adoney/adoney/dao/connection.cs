80b6a3dcf53c880360a1db543469990a20ce4ea9
Zero2220 <153716590+Zero2220@users.noreply.github.com>
2024-02-23 21:47:32 +0400

    Add files via upload

---
+++ adoney/adoney/dao/connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adoney.dao
+{
+    internal class connection
+    {
+        public const string Local = "Server=MOON12\\MAINDB;Database=Lab;Trusted_Connection=true";
+    }
+}
---
+++ adoney/adoney/tables/Events.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adoney.tables
+{
+    internal class Events
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Adress { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public 
+
+
+
+    }
+}
